f74d53759a1982f96f7394596483562e709d0b51
Abraham Brookes <abrahambrookes@gmail.com>
2024-03-29 16:21:21 +1000

    add Card component

---
+++ src/components/interface/Card.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/vue';
+const props = defineProps<{
+	title?: string;
+	subtitle?: string;
+}>();
+</script>
+
+<template>
+	<IonCard class="ion-no-margin">
+		<IonCardHeader v-if="title || subtitle || $slots.title">
+			<IonCardTitle
+				v-if="title || $slots.title"
+			>
+				{{ title }}
+				<slot name="title"></slot>
+			</IonCardTitle>
+			<IonCardSubtitle
+				v-if="subtitle"
+			>
+				{{ subtitle }}
+			</IonCardSubtitle>
+		</IonCardHeader>
+		<IonCardContent>
+			<slot></slot>
+		</IonCardContent>
+	</IonCard>
+</template>
