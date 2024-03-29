f6bc9ff137e244ce462641763bfd242f6cbce981
Abraham Brookes <abrahambrookes@gmail.com>
2024-03-29 18:46:07 +1000

    add timer edit tests

---
+++ tests/e2e/specs/editing-an-existing-timer.cy.ts
@@ -0,0 +1,196 @@
+/**
+ * On the create timer page we can fill out the form and create our timer
+ */
+import { TimerInterface } from '@stores/timerStore';
+
+let testTimer: TimerInterface
+
+describe('Editing an existing timer', () => {
+	before(() => {	
+		cy.visit('/home');
+		
+		cy.window().then((window) => {
+			window.timerStore.addTimer({
+				name: 'My new timer',
+				startAt: '04:36',
+				sound: 'bell',
+			});
+
+			testTimer = window.timerStore.timers[0];
+		});
+	});
+
+	specify('The user can view the edit screen for an existing timer', () => {
+		cy.visit('/timers/edit/' + testTimer.id);
+
+		// Fill out the form
+		cy.get('[data-testid=timer-name-input]')
+			.within(() => {
+				cy.get('input')
+				.first()
+				.should('have.value', 'My new timer')
+				.clear()
+				.type('My updated timer');
+			});
+
+		cy.get('[data-testid=timer-start-at-input]')
+			.within(() => {
+				cy.get('input')
+				.first()
+				.should('have.value', '04:36')
+				.clear()
+				.type('05:36');
+			});
+
+		// Click the save button
+		cy.ionButtonClick('[data-testid=save-timer-button]');
+
+		// Check that we have navigated to /home
+		cy.url()
+			.should('include', '/home');
+
+		// Check that the timer is displayed
+		cy.contains('My updated timer')
+			.should('exist')
+			.and('be.visible');
+		
+		cy.contains('05:36')
+			.should('exist')
+			.and('be.visible');
+		
+		// reload the page
+		cy.reload();
+
+		// Check that the timer is displayed
+		cy.contains('My updated timer')
+			.should('exist')
+			.and('be.visible');
+
+		cy.contains('05:36')
+			.should('exist')
+			.and('be.visible');
+	});
+
+	specify('Editing a timer twice in a row works as well', () => {
+		cy.visit('/timers/edit/' + testTimer.id);
+
+		// Fill out the form
+		cy.get('[data-testid=timer-name-input]')
+			.within(() => {
+				cy.get('input')
+				.first()
+				.should('have.value', 'My updated timer')
+				.clear()
+				.type('My updated timer 2');
+			});
+
+		cy.get('[data-testid=timer-start-at-input]')
+			.within(() => {
+				cy.get('input')
+				.first()
+				.should('have.value', '05:36')
+				.clear()
+				.type('06:36');
+			});
+
+		// Click the save button
+		cy.ionButtonClick('[data-testid=save-timer-button]');
+
+		// Check that we have navigated to /home
+		cy.url()
+			.should('include', '/home');
+
+		// Check that the timer is displayed
+		cy.contains('My updated timer 2')
+			.should('exist')
+			.and('be.visible');
+		
+		cy.contains('06:36')
+			.should('exist')
+			.and('be.visible');
+		
+		// click the edit button
+		cy.get('[data-testid=timer-list-item-' + testTimer.id + ']')
+			.within(() => {
+				cy.ionButtonClick('[data-testid="edit-timer-button"]');
+			});
+
+		// should redirect to the edit page
+		cy.url()
+			.should('include', '/timers/edit/' + testTimer.id);
+
+		// Fill out the form
+		cy.get('[data-testid=timer-name-input]')
+			.within(() => {
+				cy.get('input')
+				.first()
+				.should('have.value', 'My updated timer 2')
+				.clear()
+				.type('My updated timer 3');
+			})
+		
+		cy.get('[data-testid=timer-start-at-input]')
+			.within(() => {
+				cy.get('input')
+				.first()
+				.should('have.value', '06:36')
+				.clear()
+				.type('07:36');
+			})
+		
+		// Click the save button
+		cy.ionButtonClick('[data-testid=save-timer-button]');
+
+		// Check that we have navigated to /home
+		cy.url()
+			.should('include', '/home');
+
+		// Check that the timer is displayed
+		cy.contains('My updated timer 3')
+			.should('exist')
+			.and('be.visible');
+
+		cy.contains('07:36')
+			.should('exist')
+			.and('be.visible');
+	});
+
+	specify('User can cancel the edit and go back to the home screen', () => {
+		cy.visit('/timers/edit/' + testTimer.id);
+
+		// Fill out the form
+		cy.get('[data-testid=timer-name-input]')
+			.within(() => {
+				cy.get('input')
+				.first()
+				.should('have.value', 'My updated timer 3')
+				.clear()
+				.type('My updated timer 4');
+			});
+
+		cy.get('[data-testid=timer-start-at-input]')
+			.within(() => {
+				cy.get('input')
+				.first()
+				.should('have.value', '07:36')
+				.clear()
+				.type('08:36');
+			});
+
+		// Click the cancel button
+		cy.ionButtonClick('[data-testid=cancel-button]');
+
+		// Check that we have navigated to /home
+		cy.url()
+			.should('include', '/home');
+
+		// Check that the timer has not been updated
+		cy.contains('My updated timer 3')
+			.should('exist')
+			.and('be.visible');
+
+		cy.contains('07:36')
+			.should('exist')
+			.and('be.visible');
+	});
+});
---
+++ tests/e2e/specs/home-with-timers.cy.ts
@@ -0,0 +1,105 @@
+/**
+ * When the user has created some timers they will be displayed on the home screen. The user can
+ * toggle the timers on and off and edit them.
+ */
+import { TimerInterface } from '@stores/timerStore';
+
+let testTimer: TimerInterface
+
+describe('Home screen with no timers', () => {
+	before(() => {	
+		cy.visit('/home');
+		
+		cy.window().then((window) => {
+			window.timerStore.addTimer({
+				name: 'My new timer',
+				startAt: '04:36',
+				sound: 'bell',
+			});
+
+			testTimer = window.timerStore.timers[0];
+		});
+	});
+
+	after(() => {
+		cy.window().then((window) => {
+			window.timerStore.clearStore();
+		});
+	});
+
+	specify('The user should see the timer on the home screen', () => {
+		cy.visit('/home');
+
+		cy.get('[data-testid=timer-list-item-' + testTimer.id + ']')
+			.should('exist')
+			.and('be.visible')
+			.within(() => {
+				// Check that we can see the timer
+				cy.contains('My new timer')
+					.should('exist')
+					.and('be.visible');
+
+				cy.contains('04:36')
+					.should('exist')
+					.and('be.visible');
+			});
+	});
+
+	specify('The user should be able to toggle the timer using the toggle on the card and the toggle state will survive a page refresh', () => {
+		cy.visit('/home');
+
+		cy.get('[data-testid=timer-list-item-' + testTimer.id + ']')
+			.should('exist')
+			.and('be.visible')
+			.within(() => {
+				// Check that we can see the timer
+				cy.get('[data-testid="timer-enabled-toggle"]')
+					.should('exist')
+					.and('be.visible')
+					.and('have.class', 'toggle-checked')
+					.click();
+			});
+		
+		cy.reload();
+
+		cy.get('[data-testid=timer-list-item-' + testTimer.id + ']')
+			.should('exist')
+			.and('be.visible')
+			.within(() => {
+				cy.get('[data-testid="timer-enabled-toggle"]')
+					.should('exist')
+					.and('be.visible')
+					.and('not.have.class', 'toggle-checked')
+					.click();
+			});
+		
+		cy.reload();
+
+		cy.get('[data-testid=timer-list-item-' + testTimer.id + ']')
+			.should('exist')
+			.and('be.visible')
+			.within(() => {
+				// Check that we can see the timer
+				cy.get('[data-testid="timer-enabled-toggle"]')
+					.should('exist')
+					.and('be.visible')
+					.and('have.class', 'toggle-checked');
+			});
+	});
+
+	specify('The user should be able to edit the timer from the card on the home screen', () => {
+		cy.visit('/home');
+
+		cy.get('[data-testid=timer-list-item-' + testTimer.id + ']')
+			.should('exist')
+			.and('be.visible')
+			.within(() => {
+				// Click the edit button
+				cy.ionButtonClick('[data-testid="edit-timer-button"]');
+
+				// Check that we have navigated to /timers/edit
+				cy.url()
+					.should('include', '/timers/edit/' + testTimer.id);
+			});
+	});
+});
