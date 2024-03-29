d5810d9bd1e3bacf69f28e34fd47f1cb6db81483
Abraham Brookes <abrahambrookes@gmail.com>
2024-03-29 13:40:37 +1000

    global import commonly used ion components

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import router from './router';
 
 import { IonicVue } from '@ionic/vue';
 
+import {
+	IonButton,
+	IonButtons,
+	IonContent,
+	IonHeader,
+	IonMenuButton,
+	IonPage,
+	IonTitle,
+	IonToolbar
+} from '@ionic/vue';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
@@ -25,7 +36,15 @@ import './theme/variables.css';
 
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .component('IonPage', IonPage)
+  .component('IonHeader', IonHeader)
+  .component('IonContent', IonContent)
+  .component('IonButtons', IonButtons)
+  .component('IonButton', IonButton)
+  .component('IonMenuButton', IonMenuButton)
+  .component('IonTitle', IonTitle)
+  .component('IonToolbar', IonToolbar);
   
 router.isReady().then(() => {
   app.mount('#app');
