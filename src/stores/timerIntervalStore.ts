be69f4ece6c1e39915e655093f21c92a77e55f4a
Abraham Brookes <abrahambrookes@gmail.com>
2024-03-31 23:43:38 +1000

    add logic to schedule notifications when enabling a timer from the main screen and cancel them when disabling

--- src/stores/timerIntervalStore.ts
+++ src/stores/timerIntervalStore.ts
@@ -1,6 +1,12 @@
 import { defineStore } from 'pinia';
 import { Storage } from '@ionic/storage';
 import { v4 as uuidv4 } from 'uuid';
+import { TimerInterface, useTimerStore } from './timerStore';
+import {
+	LocalNotificationDescriptor,
+	LocalNotifications,
+	ScheduleResult
+} from '@capacitor/local-notifications';
 
 // TimerInterval is the subsequent alerts that are played after the initial timerInterval
 export interface TimerIntervalInterface {
@@ -8,6 +14,7 @@ export interface TimerIntervalInterface {
 	timer_id: string;
 	duration: number|null;
 	sound: string;
+	notification?: LocalNotificationDescriptor;
 }
 
 export const useTimerIntervalStore = defineStore('timerIntervalStore', {
@@ -17,6 +24,7 @@ export const useTimerIntervalStore = defineStore('timerIntervalStore', {
 		timerIntervals: [] as TimerIntervalInterface[],
 	}),
 	actions: {
+		// given a timerInterval object, add it to the store
 		async addTimerInterval(timerInterval: TimerIntervalInterface) {
 			this.timerIntervals.push({
 				...timerInterval,
@@ -25,6 +33,8 @@ export const useTimerIntervalStore = defineStore('timerIntervalStore', {
 
 			await this.persistStore();
 		},
+
+		// given a timerInterval object, update it in the store
 		async updateTimerInterval(timerInterval: TimerIntervalInterface) {
 			const index = this.timerIntervals.findIndex(t => t.id === timerInterval.id);
 			if (index >= 0) {
@@ -33,6 +43,8 @@ export const useTimerIntervalStore = defineStore('timerIntervalStore', {
 
 			await this.persistStore();
 		},
+
+		// given a timerInterval object, remove it from the store
 		async removeTimerInterval(remove: TimerIntervalInterface) {
 			this.timerIntervals = this.timerIntervals.filter(timerInterval => {
 				return timerInterval !== remove;
@@ -40,16 +52,80 @@ export const useTimerIntervalStore = defineStore('timerIntervalStore', {
 
 			await this.persistStore();
 		},
+
+		// given a timerInterval id, find the timerInterval
 		find(id: string): TimerIntervalInterface | undefined {
 			return this.timerIntervals.find(t => t.id === id);
 		},
+
+		// given a timer id, return all the timerIntervals for that timer
 		getForTimer(timer_id: string): TimerIntervalInterface[] {
 			return this.timerIntervals.filter(t => t.timer_id === timer_id);
 		},
+
+		// given a timer id, remove all the timerIntervals for that timer
 		removeForTimer(timer_id: string): TimerIntervalInterface[] {
 			this.timerIntervals = this.timerIntervals.filter(t => t.timer_id !== timer_id);
 			return this.timerIntervals;
 		},
+		
+		// given a timer id, schedule all the notifications for the timerIntervals
+		scheduleNotificationsForTimer(timer_id: string) {
+			const timerStore = useTimerStore();
+			const timerIntervals = this.getForTimer(timer_id);
+
+			timerIntervals.forEach(async (timerInterval: TimerIntervalInterface) => {
+				const index = this.timerIntervals.findIndex(t => t.timer_id === timer_id);
+
+				const targetTime = this.startTime(timerInterval);
+
+				LocalNotifications.schedule({
+					notifications: [
+						{
+							title: 'Hypnogogic',
+							body: 'Timer interval alert',
+							id: index + timerStore.timers.length + 1,
+							schedule: { at: targetTime },
+							channelId: timerInterval.timer_id,
+						}
+					]
+				})
+				.then((result: ScheduleResult) => {
+					this.timerIntervals[index].notification = result.notifications[0];
+				})
+				.catch((e) => {
+					alert('Failed to schedule notification: ' + e.message);
+					throw 'Failed to schedule notification: ' + e.message;
+				});
+			});
+
+			this.persistStore();
+		},
+
+		// given a timer id, cancel all the notifications for the timerIntervals
+		async cancelNotificationsForTimer(timer_id: string) {
+			const timerStore = useTimerStore();
+			const timerIntervals = this.getForTimer(timer_id);
+
+			timerIntervals.forEach(async (timerInterval: TimerIntervalInterface) => {
+				const index = this.timerIntervals.findIndex(t => t.timer_id === timer_id);
+
+				if (timerInterval.notification) {
+					await LocalNotifications.cancel({
+						notifications: [timerInterval.notification]
+					}).catch((e) => {
+						alert('Failed to cancel notification: ' + e.message);
+						throw 'Failed to cancel notification: ' + e.message;
+					});
+
+					this.timerIntervals[index].notification = undefined;
+				}
+
+				this.persistStore();
+			});
+		},
+
+		// persist the store to the device
 		async persistStore() {
 			this.saving = true;
 			const store = new Storage();
@@ -57,6 +133,8 @@ export const useTimerIntervalStore = defineStore('timerIntervalStore', {
 			await store.set('timerIntervalstore', JSON.stringify(this.timerIntervals));
 			this.saving = false;
 		},
+
+		// restore the store from the device
 		async restoreStore() {
 			this.loading = true;
 			const store = new Storage();
@@ -67,9 +145,31 @@ export const useTimerIntervalStore = defineStore('timerIntervalStore', {
 			}
 			this.loading = false;
 		},
+
+		// clear the store
 		async clearStore() {
 			this.timerIntervals = [];
 			await this.persistStore();
+		}
+	},
+
+	getters: {
+		// given a timerInterval object, calculate the time when the notification should be scheduled
+		startTime: (state) => (timerInterval: TimerIntervalInterface): Date => {
+			const timerStore = useTimerStore();
+			const timer: TimerInterface | undefined = timerStore.find(timerInterval.timer_id);
+			if (!timer) {
+				throw 'Timer not found';
+			}
+		
+			const targetTime = timerStore.startTime(timer);
+			if (timerInterval.duration) {
+				targetTime.setMinutes(targetTime.getMinutes() + timerInterval.duration);
+
+				return targetTime;
+			}
+
+			throw 'Could not calculate start time for timerInterval';
 		}
 	}
 });
--- src/stores/timerStore.ts
+++ src/stores/timerStore.ts
@@ -2,6 +2,13 @@ import { defineStore } from 'pinia';
 import { Storage } from '@ionic/storage';
 import { v4 as uuidv4 } from 'uuid';
 import { SoundInterface } from '@stores/soundStore';
+import { useTimerIntervalStore } from '@stores/timerIntervalStore';
+import {
+	LocalNotificationDescriptor,
+	LocalNotifications,
+	ScheduleResult
+} from '@capacitor/local-notifications';
+import { DateTime } from 'luxon';
 
 // the Timer is the main object we are storing in the store
 export interface TimerInterface {
@@ -10,6 +17,7 @@ export interface TimerInterface {
 	start_at: string;
 	sound: SoundInterface;
 	enabled: boolean;
+	notification?: LocalNotificationDescriptor;
 }
 
 export const useTimerStore = defineStore('timerStore', {
@@ -19,6 +27,7 @@ export const useTimerStore = defineStore('timerStore', {
 		timers: [] as TimerInterface[],
 	}),
 	actions: {
+		// given a timer object, add it to the store
 		async addTimer(timer: TimerInterface): Promise<TimerInterface> {
 			this.timers.push({
 				...timer,
@@ -30,6 +39,8 @@ export const useTimerStore = defineStore('timerStore', {
 
 			return this.timers[this.timers.length - 1];
 		},
+
+		// given a timer object, update it in the store
 		async updateTimer(timer: TimerInterface) {
 			const index = this.timers.findIndex(t => t.id === timer.id);
 			if (index >= 0) {
@@ -38,6 +49,8 @@ export const useTimerStore = defineStore('timerStore', {
 
 			await this.persistStore();
 		},
+
+		// given a timer id, remove it from the store
 		async removeTimer(id: string) {
 			this.timers = this.timers.filter(timer => {
 				return timer.id !== id;
@@ -45,17 +58,79 @@ export const useTimerStore = defineStore('timerStore', {
 
 			await this.persistStore();
 		},
+
+		// given a timer id, find the timer
 		find(id: string): TimerInterface | undefined {
 			return this.timers.find(t => t.id === id);
 		},
+
+		// given a timer object, toggle the enabled state and handle notifications
 		async toggleTimerEnabled(timer: TimerInterface, enabled: boolean) {
 			const index = this.timers.findIndex(t => t.id === timer.id);
-			if (index >= 0) {
-				this.timers[index].enabled = enabled;
+
+			if (index < 0) {
+				return;
+			}
+
+			let selectedTimer = this.timers[index];
+			selectedTimer.enabled = enabled;
+			
+			if(enabled) {
+				LocalNotifications.requestPermissions();
+			}
+
+			// if no channel, create one
+			checkAndCreateNotificationChannel(selectedTimer);
+
+			// if the timer is disabled, cancel the notification
+			if (!enabled && selectedTimer.notification) {
+				await LocalNotifications.cancel({
+					notifications: [selectedTimer.notification]
+				}).catch((e) => {
+					alert('Failed to cancel notification: ' + e.message);
+				});
+
+				const timerIntervalStore = useTimerIntervalStore();
+				await timerIntervalStore.cancelNotificationsForTimer(selectedTimer.id);
+
+				selectedTimer.notification = undefined;
+			}
+
+			// if the timer is enabled, schedule the notification
+			if (enabled) {
+				const targetTime = this.startTime(selectedTimer);
+
+				// schedule the notification
+				LocalNotifications.schedule({
+					notifications: [
+						{
+							title: 'Hypnogogic alert',
+							body: 'Playing ' + selectedTimer.sound.name,
+							id: index,
+							schedule: { at: targetTime },
+							channelId: selectedTimer.id,
+						}
+					]
+				})
+				.then((result: ScheduleResult) => {
+					// save the notification to the timer so we can cancel it later
+					this.timers[index].notification = result.notifications[0];
+				})
+				.then(() => {
+					// schedule the timer intervals
+					const timerIntervalStore = useTimerIntervalStore();
+					timerIntervalStore.scheduleNotificationsForTimer(selectedTimer.id);
+				})
+				.catch((e) => {
+					alert('Failed to schedule notification: ' + e.message);
+					throw 'Failed to schedule notification: ' + e.message;
+				});
 			}
 
 			await this.persistStore();
 		},
+
+		// persist the store to storage
 		async persistStore() {
 			this.saving = true;
 			const store = new Storage();
@@ -63,6 +138,8 @@ export const useTimerStore = defineStore('timerStore', {
 			await store.set('timerStore', JSON.stringify(this.timers));
 			this.saving = false;
 		},
+
+		// restore the store from storage
 		async restoreStore() {
 			this.loading = true;
 			const store = new Storage();
@@ -73,9 +150,50 @@ export const useTimerStore = defineStore('timerStore', {
 			}
 			this.loading = false;
 		},
+
+		// clear the store
 		async clearStore() {
 			this.timers = [];
 			await this.persistStore();
 		}
+	},
+
+	getters: {
+		// given a timer, return the start time
+		startTime: (state) => (timer: TimerInterface) : Date => {
+			const [hours, minutes] = timer.start_at.split(':').map(Number);
+			let targetTime = DateTime.now().set({ hour: hours, minute: minutes, second: 0, millisecond: 0 });
+		
+			// If the target time is in the past (earlier today), add one day to schedule for tomorrow
+			if (targetTime <= DateTime.now()) {
+				targetTime = targetTime.plus({ days: 1 });
+			}
+		
+			return targetTime.toJSDate();
+		}
 	}
 });
+
+// check if the notification channel exists, and create it if it doesn't
+function checkAndCreateNotificationChannel(timer: TimerInterface) {
+	LocalNotifications.listChannels().then((channels) => {
+		let channel = channels.channels.find((channel) => channel.id === timer.id);
+
+		if (!channel) {
+			LocalNotifications.createChannel({
+				id: timer.id,
+				name: timer.name,
+				description: 'Hypnogogic alarm channel',
+				importance: 5,
+				sound: timer.sound.src,
+				vibration: false
+			}).catch((e) => {
+				alert('Failed to create channel: ' + e.message);
+				throw 'Failed to create channel: ' + e.message;
+			});
+		}
+	}).catch((e) => {
+		alert('Failed to list channels: ' + e.message);
+		throw 'Failed to list channels: ' + e.message;
+	})
+}
