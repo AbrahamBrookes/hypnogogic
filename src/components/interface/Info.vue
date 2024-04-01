9d0c8fc8dba235558e2203ed4276b8d6cf1221b6
Abraham Brookes <abrahambrookes@gmail.com>
2024-04-01 21:16:07 +1000

    add info component

---
+++ src/components/interface/Info.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Card from '@components/interface/Card.vue';
+</script>
+
+<template>
+	<Card
+		style="
+			background: var(--ion-color-danger);
+			color: var(--ion-color-light);
+		"
+	>
+		<slot></slot>
+	</Card>
+</template>
